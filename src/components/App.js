fd8de7822b353e5715284b173a68e96f49e4a679
Bogdan <thegarrybit@gmail.com>
2023-06-28 14:56:34 +0300

    refactor: lift up split bill state to parent component

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,36 @@ import INITIAL_FRIENDS from '../config';
 
 export default function App() {
   const [openedBill, setOpenedBill] = useState(null);
+  const [billVal, setBillVal] = useState('');
+  const [expenseVal, setExpenseVal] = useState('');
+  const [whoPay, setWhoPay] = useState('You');
+
+  const friendExpense = +billVal - +expenseVal;
   const openedBillFriendName = INITIAL_FRIENDS.find(
     ({ id }) => id === openedBill,
   )?.name;
+
+  /**
+   * @param newVal {string}
+   * @returns {boolean}
+   */
+  function valIsNotNumber(newVal) {
+    return newVal !== '' && !Number(newVal);
+  }
+
+  function handleBillVal(newVal) {
+    if (valIsNotNumber(newVal)) return;
+    setBillVal(newVal);
+  }
+
+  function handleExpenseVal(newVal) {
+    if (Number(newVal) > billVal || valIsNotNumber(newVal)) return;
+    setExpenseVal(Math.abs(newVal));
+  }
+
+  function handleWhoPay(val) {
+    setWhoPay(val);
+  }
 
   function handleOpenedBill(id) {
     if (openedBill === id) return setOpenedBill(null);
@@ -17,7 +44,18 @@ export default function App() {
   return (
     <main className="app">
       <FriendList onBillOpen={handleOpenedBill} openedBill={openedBill} />
-      {openedBill && <SplitBillForm friendName={openedBillFriendName} />}
+      {openedBill && (
+        <SplitBillForm
+          whoPay={whoPay}
+          billVal={billVal}
+          friendExpense={friendExpense}
+          expenseVal={expenseVal}
+          onBillVal={handleBillVal}
+          onExpenseVal={handleExpenseVal}
+          onWhoPay={handleWhoPay}
+          friendName={openedBillFriendName}
+        />
+      )}
     </main>
   );
 }
--- src/components/Forms/SplitBillForm.js
+++ src/components/Forms/SplitBillForm.js
@@ -1,34 +1,15 @@
 import Button from '../Button';
-import { useState } from 'react';
 
-export default function SplitBillForm({ friendName }) {
-  const [billVal, setBillVal] = useState('');
-  const [expenseVal, setExpenseVal] = useState('');
-  const [whoPay, setWhoPay] = useState('You');
-  const friendExpense = +billVal - +expenseVal;
-
-  /**
-   * @param newVal {string}
-   * @returns {boolean}
-   */
-  function valIsNotNumber(newVal) {
-    return newVal !== '' && !Number(newVal);
-  }
-
-  function handleBillVal(newVal) {
-    if (valIsNotNumber(newVal)) return;
-    setBillVal(newVal);
-  }
-
-  function handleExpenseVal(newVal) {
-    if (Number(newVal) > billVal || valIsNotNumber(newVal)) return;
-    setExpenseVal(Math.abs(newVal));
-  }
-
-  function handleWhoPay(val) {
-    setWhoPay(val);
-  }
-
+export default function SplitBillForm({
+  friendName,
+  whoPay,
+  billVal,
+  expenseVal,
+  friendExpense,
+  onBillVal,
+  onExpenseVal,
+  onWhoPay,
+}) {
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {friendName}</h2>
@@ -36,14 +17,14 @@ export default function SplitBillForm({ friendName }) {
       <label>💰 Bill value</label>
       <input
         value={billVal}
-        onChange={(e) => handleBillVal(e.target.value)}
+        onChange={(e) => onBillVal(e.target.value)}
         type="text"
       />
 
       <label>🧍‍♀️ Your expense</label>
       <input
         value={expenseVal}
-        onChange={(e) => handleExpenseVal(e.target.value)}
+        onChange={(e) => onExpenseVal(e.target.value)}
         type="text"
       />
 
@@ -51,7 +32,7 @@ export default function SplitBillForm({ friendName }) {
       <input value={friendExpense} type="text" disabled={true} />
 
       <label>🤑 Who is paying the bill</label>
-      <select value={whoPay} onChange={(e) => handleWhoPay(e.target.value)}>
+      <select value={whoPay} onChange={(e) => onWhoPay(e.target.value)}>
         <option value="You">You</option>
         <option value="Friend">{friendName}</option>
       </select>
