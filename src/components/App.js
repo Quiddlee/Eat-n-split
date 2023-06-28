b2372f1af99a6669bd575aaf7eced5a4a1ac2b25
Bogdan <thegarrybit@gmail.com>
2023-06-28 12:51:32 +0300

    refactor: lift up open bill state

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import FriendList from './FriendList/FriendList';
+import SplitBillForm from './SplitBillForm';
+import { useState } from 'react';
 
 export default function App() {
+  const [openedBill, setOpenedBill] = useState(null);
+
+  function handleOpenedBill(id) {
+    if (openedBill === id) return setOpenedBill(null);
+    setOpenedBill(id);
+  }
+
   return (
     <main className="app">
-      <FriendList />
+      <FriendList onBillOpen={handleOpenedBill} openedBill={openedBill} />
+      <SplitBillForm />
     </main>
   );
 }
--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,22 +1,14 @@
 import ListItem from './ListItem';
 import INITIAL_FRIENDS from '../../config';
-import { useState } from 'react';
 import Button from '../Button';
 
-export default function FriendList() {
-  const [openedBill, setOpenedBill] = useState(null);
-
-  function handleOpenedBill(id) {
-    if (openedBill === id) return setOpenedBill(null);
-    setOpenedBill(id);
-  }
-
+export default function FriendList({ onBillOpen, openedBill }) {
   return (
     <article className="sidebar">
       <ul>
         {INITIAL_FRIENDS.map((data) => (
           <ListItem key={data.id} id={data.id} data={data}>
-            <Button onClick={handleOpenedBill.bind(null, data.id)}>
+            <Button onClick={onBillOpen.bind(null, data.id)}>
               {openedBill === data.id ? 'Close' : 'Select'}
             </Button>
           </ListItem>
